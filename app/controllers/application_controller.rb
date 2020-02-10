283051d53c5a0e395d0d155712db54e76a18aed3
Chikako Sugano <chikasienne@gmail.com>
2020-02-10 05:03:45 +0000

    Finish edit-attendances

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,24 +3,47 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
 
   $days_of_the_week = %w{日 月 火 水 木 金 土}
+  
+  #beforフィルター
+  
+  #paramsハッシュからユーザーを取得します。
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  #ログイン済みのユーザーか確認
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "ログインしてください。"
+      redirect_to login_url
+    end
+  end
+  
+  #アクセスしたユーザーが現在ログインしているユーザーか確認
+  def admin_user
+    redirect_to root_url unless current_user.admin?
+  end
 
   # ページ出力前に1ヶ月分のデータの存在を確認・セットします。
   def set_one_month 
     @first_day = params[:date].nil? ?
     Date.current.beginning_of_month : params[:date].to_date
     @last_day = @first_day.end_of_month
-    one_month = [*@first_day..@last_day]
-  
+    one_month = [*@first_day..@last_day] #対象の月の日数を代入
+    
+    #ユーザーに紐付く一ヶ月分のレコードを検索し取得
     @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
   
-    unless one_month.count == @attendances.count
-      ActiveRecord::Base.transaction do
+    unless one_month.count == @attendances.count #それぞれの件数(日数)が一致するか評価します。
+      ActiveRecord::Base.transaction do #トランザクションを開始します。
+        #繰り返し処理により、1ヶ月分の勤怠データを生成
         one_month.each { |day| @user.attendances.create!(worked_on: day) }
       end
       @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
     end
   
-  rescue ActiveRecord::RecordInvalid
+  rescue ActiveRecord::RecordInvalid　#トランザクションによるエラーの分岐
     flash[:danger] = "ページ情報の取得に失敗しました、再アクセスしてください。"
     redirect_to root_url
   end
